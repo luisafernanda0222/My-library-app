5742d0483df392620ecd62579b41460248bd510a
luisa <SenaManana@ARMDFPCCIFSW013.FormacionSena.RED>
2024-06-13 09:13:08 -0500

    tipado primitivo

---
+++ src/variables-types.ts
@@ -0,0 +1,26 @@
+let bookTitle: string= "TypeScript Basics";
+
+const libraryName: string= "My Library";
+
+let numberOfBooks: number= 3;
+
+let isLibraryOpen: boolean= true;
+
+
+
+let titulo: string = " Julio César";
+let autor: string= "William Shakespeare";
+let genero: string= "traedia";
+let idioma: string= "aleman";
+let precio: string= "66.790";
+let formato: string= "tapa blanda";
+let isbn: number= 9780133555387;
+let descipcion: string= "Recrea la conspiración en contra del dictador romano, su homicidio y sus secuelas, así como su lugar de desarrollo: la antigua Roma";
+let estado: string= "nuevo";
+let ubicacion: string= "buscaLibre";
+let fecha_publicacion:"1623";
+let editorial: string= "Penguin Random House";
+let paginas: number= 105;
+let dimensiones:string= "12.5 x 19 cm";
+let peso: string = "50 kg";
+let disponible: boolean= true;
